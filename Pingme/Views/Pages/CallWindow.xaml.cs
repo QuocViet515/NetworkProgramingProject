0887ab746bdc08deacb255ce1a9e8de3a545c68a
QuocViet515 <vietpham05012005@gmail.com>
2025-05-21 23:44:36 +0700

    build test video call 3

--- Pingme/Views/Pages/CallWindow.xaml.cs
+++ Pingme/Views/Pages/CallWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using Pingme.Services;
+using System;
 
 namespace Pingme.Views.Pages
 {
     public partial class CallWindow : Window
     {
-        private string _appId;
-        private string _channelName;
+        private readonly string _appId;
+        private readonly string _channelName;
 
         private readonly AgoraVideoService _videoService;
 
@@ -14,20 +15,37 @@ namespace Pingme.Views.Pages
         {
             InitializeComponent();
 
-            _videoService = new AgoraVideoService(LocalVideoContainer, RemoteVideoContainer); // truyền container
-            _videoService.InitializeAgora(appId, channel);
+            _appId = appId;
+            _channelName = channel;
+
+            // Khởi tạo dịch vụ video, truyền 2 container
+            _videoService = new AgoraVideoService(LocalVideoContainer, RemoteVideoContainer);
+
+            Loaded += CallWindow_Loaded;
+            Closed += CallWindow_Closed;
         }
 
-        private void JoinCall_Click(object sender, RoutedEventArgs e)
+        private void CallWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            _videoService.InitializeAgora(_appId, _channelName);
-            var videoElement = _videoService.GetVideoControl();
-            VideoContainer.Content = videoElement;
+            try
+            {
+                _videoService.InitializeAgora(_appId, _channelName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ Lỗi khi khởi tạo cuộc gọi: " + ex.Message);
+            }
         }
 
-        public void SetVideoControl(UIElement videoElement)
+        private void CallWindow_Closed(object sender, EventArgs e)
         {
-            VideoContainer.Content = videoElement;
+            _videoService.LeaveChannel(); // Dọn dẹp khi đóng cửa sổ
         }
+
+        // (Nếu bạn vẫn dùng VideoContainer để test riêng thì giữ lại, còn không thì có thể bỏ)
+        //public void SetVideoControl(UIElement videoElement)
+        //{
+        //    VideoContainer.Content = videoElement;
+        //}
     }
 }
